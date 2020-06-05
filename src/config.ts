fca51188bedd872dc72606a0c5f5a3974dd60615
Jordi <jordigranadarubio@gmail.com>
2020-06-05 17:40:08 +0200

    change ports

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const config = {
         cors: {
             server: [
                 {
-                    origin: 'localhost:3000', //servidor que deseas que consuma o (*) en caso que sea acceso libre
+                    origin: 'localhost:4200', //servidor que deseas que consuma o (*) en caso que sea acceso libre
                     credentials: true,
                 },
             ],
